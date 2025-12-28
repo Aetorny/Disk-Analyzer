5ef086d7b2651b6c1ada4707c7666253f6a7d1cf
Aetorny <smgusak@gmail.com>
2025-12-29 00:10:33 +0300

    change lang to rus, open files after complete

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from glob import glob
 import os
 
-
 from get_size import SizeFinder
 import visualizer
-from info import DATA_DIR, PLATFORM
+from info import DATA_DIR
 
 
 def main() -> None:
@@ -13,15 +12,16 @@ def main() -> None:
 
     visualizer.main()
 
-    print('Visualization complete.')
+    print('Визуализация завершена.')
     files = glob('*.html', root_dir=DATA_DIR)
-    print('Generated visualization files:')
+    print('Сгенерированные файлы:')
     for idx, file in enumerate(files):
-        print(f'{idx+1}: {os.path.join(DATA_DIR, file)}')
+        print(f'\t{idx+1}: {os.path.join(DATA_DIR, file)}')
     
-    if PLATFORM == 'Windows' and files:
-        os.startfile(DATA_DIR)
-    input('Press Enter to exit...')
+    input('Нажмите Enter для открытия сгенерированных файлов...')
+
+    for file in files:
+        os.startfile(os.path.join(DATA_DIR, file))
 
 
 if __name__ == '__main__':
