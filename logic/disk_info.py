a633a76ce8bcdd34c339bc62492521d698768951
Aetorny <smgusak@gmail.com>
2026-01-09 23:38:37 +0300

    add comments

--- logic/disk_info.py
+++ logic/disk_info.py
@@ -5,6 +5,7 @@ from config import PLATFORM
 
 
 def get_drives_on_windows() -> list[str]:
+    '''Возвращает диски, которые доступны в системе (только для Windows)'''
     import string
     import ctypes
     drives: list[str] = []
@@ -18,6 +19,7 @@ def get_drives_on_windows() -> list[str]:
 
 
 def get_start_directories() -> list[str]:
+    '''Возвращает начальные директории'''
     if PLATFORM == "Windows":
         return get_drives_on_windows()
     else:
@@ -25,10 +27,12 @@ def get_start_directories() -> list[str]:
 
 
 def get_used_disk_size(path: str) -> int:
+    '''Возвращает используемый объем диска'''
     _, used, _ = shutil.disk_usage(path)
     logging.info(f'Получен объем диска {path}: {used}')
     return used
 
 
 def is_root(path: str) -> bool:
+    '''Проверяет является ли текущий путь корнем'''
     return path == os.path.dirname(path)
