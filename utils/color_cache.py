f5c3711aaefbc3d31af3cfe2dbac92b8011ff4e6
Aetorny <smgusak@gmail.com>
2026-01-13 00:25:28 +0300

    add new color scheme to visualize

--- utils/color_cache.py
+++ utils/color_cache.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 
 import math
+import logging
 
 
 class ColorCache:
     def __init__(self, cmap_name: str, steps: int = 512):
-        self.cmap = plt.get_cmap(cmap_name)
+        logging.info(f"Цветовая схема: {cmap_name}")
+        try:
+            self.cmap = plt.get_cmap(cmap_name)
+        except ValueError:
+            self.colors_rgb = [(50, 50, 50)]
+            self.steps = 0
+            logging.warning(f"Цветовая схема не найдена: {cmap_name}")
+            return
         self.steps = steps
         self.colors_rgb: list[tuple[int, int, int]] = []
         self.text_colors_hex: list[str] = [] 
@@ -19,11 +27,7 @@ class ColorCache:
             g = int(rgba[1] * 255)
             b = int(rgba[2] * 255)
             self.colors_rgb.append((r, g, b))
-            
-            # Яркость
-            luminance = 0.299*rgba[0] + 0.587*rgba[1] + 0.114*rgba[2]
-            self.text_colors_hex.append("#000000" if luminance > 0.5 else "#FFFFFF")
-    
+
     def get_color_rgb_and_text(self, size_bytes: float, global_max_log: float) -> tuple[int, int, int]:
         if size_bytes <= 0:
             return (50, 50, 50)
@@ -40,3 +44,19 @@ class ColorCache:
         
         safe_idx = max(0, min(idx, self.steps - 1))
         return self.colors_rgb[safe_idx]
+
+    def get_rgb_by_number(self, number: int) -> tuple[int, int, int]:
+        """
+        Принимает число и возвращает кортеж (R, G, B) в диапазоне 0-255.
+        """
+        cmap = plt.get_cmap('tab20')
+        
+        index = int(number) % cmap.N
+        
+        rgba = cmap(index)
+        
+        r = int(rgba[0] * 255)
+        g = int(rgba[1] * 255)
+        b = int(rgba[2] * 255)
+        
+        return r, g, b
